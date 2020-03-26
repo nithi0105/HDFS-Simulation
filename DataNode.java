a2880c7f9d7804859e2c6e8bd335433253b1994e
nithi0105 <38299258+nithi0105@users.noreply.github.com>
2020-03-25 20:18:53 -0400

    Add files via upload

---
+++ DataNode.java
@@ -0,0 +1,191 @@
+//Written By Shaleen Garg
+package ds.hdfs;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.*;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+//import ds.hdfs.hdfsformat.*;
+import ds.hdfs.IDataNode.*;
+
+public class DataNode implements IDataNode
+{
+	HdfsDefn.DataNode.Builder response = HdfsDefn.DataNode.newBuilder(); //temp
+    protected String MyChunksFile;
+    protected INameNode NNStub;
+    protected String MyIP;
+    protected int MyPort;
+    protected String MyName;
+    protected int MyID;
+
+    public DataNode(String name, int storagePort, String IP)
+    {
+        //Constructor
+    	this.MyName = name;
+    	this.MyPort = storagePort;
+        this.MyIP = IP;
+    }
+
+    /*public static void appendtoFile(String Filename, String Line)
+    {
+        BufferedWriter bw = null;
+
+        try {
+            //append
+        } 
+        catch (IOException ioe) 
+        {
+            ioe.printStackTrace();
+        } 
+        finally 
+        {                       // always close the file
+            if (bw != null) try {
+                bw.close();
+            } catch (IOException ioe2) {
+            }
+        }
+
+    }*/
+
+    public String[] readConfig(String filename){
+        BufferedReader objReader = null;
+        try {
+            String strCurrentLine;
+
+            objReader = new BufferedReader(new FileReader(filename));
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+            ArrayList<String> configDetails = new ArrayList<String>();
+            configDetails.add(strCurrentLine);
+    	    if(configDetails.size()>0) {
+    		String to_split = configDetails[0];
+    		String[] config_split = to_split.split(";");
+            //System.out.println(strCurrentLine);
+        }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+        try {
+            if (objReader != null)
+            objReader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        }
+        return config_split;
+}
+
+    public byte[] readBlock(byte[] Inp)
+    {
+        try
+        {
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error at readBlock");
+            //response.setStatus(-1);
+        }
+
+        return response.build().toByteArray();
+    }
+
+    public byte[] writeBlock(byte[] Inp)
+    {
+        try
+        {
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error at writeBlock ");
+            //response.setStatus(-1);
+        }
+
+        return response.build().toByteArray();
+    }
+
+    public void BlockReport() throws IOException
+    {
+    }
+
+    public void BindServer(String Name, String IP, int Port)
+    {
+        try
+        {
+            IDataNode stub = (IDataNode) UnicastRemoteObject.exportObject(this, 0);
+            System.setProperty("java.rmi.server.hostname", IP);
+            Registry registry = LocateRegistry.createRegistry(Port);
+            registry.rebind(Name, stub);
+            System.out.println("\nDataNode connected to RMIregistry\n");
+        }catch(Exception e){
+            System.err.println("Server Exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public INameNode GetNNStub(String filename)
+    {
+        while(true)
+        {
+            try
+            {
+                String [] config_split = readConfig(filename);
+                String Name = config_split[0];
+                String IP = config_split[1];
+                int Port = config_split[2];
+                Registry registry = LocateRegistry.getRegistry(IP, Port);
+                INameNode stub = (INameNode) registry.lookup(Name);
+                System.out.println("NameNode Found!");
+                return stub;
+            }catch(Exception e){
+                System.out.println("NameNode still not Found");
+                continue;
+            }
+        }
+    }
+
+    /*public INameNode GetNNStub(String Name, String IP, int Port)
+    {
+        while(true)
+        {
+            try
+            {
+                String [] config_split = readConfig("nn_config.txt");
+
+                Registry registry = LocateRegistry.getRegistry(IP, Port);
+                INameNode stub = (INameNode) registry.lookup(Name);
+                System.out.println("NameNode Found!");
+                return stub;
+            }catch(Exception e){
+                System.out.println("NameNode still not Found");
+                continue;
+            }
+        }
+    }*/
+
+    public static void main(String args[]) throws InvalidProtocolBufferException, IOException
+    {
+        //Define a Datanode Me
+        DataNode Me = new DataNode("cp", 2005, "128.6.13.177"); //get from dn config?
+        INameNode stub = Me.GetNNStub("nn_config.txt"); //get from nn config?
+        Me.NNStub = stub;
+        Me.BindServer("IDataNode", "128.6.13.177", 2005); //get from config
+
+    }
+}
