6991d28d5d9fe00d13e85a1f425763e7003139a1
nithi0105 <38299258+nithi0105@users.noreply.github.com>
2020-04-02 10:13:35 -0400

    Update Client.java

--- Project2/MAPR/src/ds/hdfs/Client.java
+++ Project2/MAPR/src/ds/hdfs/Client.java
@@ -17,6 +17,7 @@ import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 
 public class Client
